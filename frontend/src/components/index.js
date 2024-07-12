a3b47f09587176cfa895dcd83ea14bf7a41ba060
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2024-07-13 02:49:34 +0300

    Present product gallery as an image carousel

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -6,3 +6,5 @@ export { default as ProductAttributes } from './ProductAttributes';
 export { default as CartModal } from './cart/CartModal';
 export { default as CartModalItem } from './cart/CartModalItem';
 export { default as ActionBtn } from './cart/ActionBtn';
+export { default as Arrow } from './svgs/Arrow';
+export { default as ProductImageCarousel } from './ProductImageCarousel';
