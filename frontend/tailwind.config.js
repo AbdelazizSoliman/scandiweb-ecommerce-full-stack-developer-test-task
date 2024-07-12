7be89d9a487886addffb8ace020eef6af68541b7
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2024-07-12 11:39:06 +0300

    Update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'system-ui', 'sans-serif'],
+        roboto: ['Roboto', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        accent: '#5ECE7B',
+        text: '#1D1F22',
+      },
+    },
   },
   plugins: [],
-}
-
+};
