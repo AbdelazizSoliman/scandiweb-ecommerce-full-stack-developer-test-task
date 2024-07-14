19c4ded44bc1607c933da5f6bcfcef09c86b41e6
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2024-07-14 07:05:10 +0300

    Add Loading component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -8,3 +8,7 @@ export { default as ProductImageCarousel } from './ProductImageCarousel';
 export { default as CartModal } from './cart/CartModal';
 export { default as CartModalItem } from './cart/CartModalItem';
 export { default as ActionBtn } from './cart/ActionBtn';
+export { default as Spinner } from './svgs/Spinner';
+export { default as NavigationMenu } from './NavigationMenu';
+export { default as Loading } from './Loading';
+
