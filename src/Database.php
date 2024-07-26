b8effb695b75d9ad875009a41d506616b38bfb1e
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2024-07-26 20:46:38 +0300

    Update dsn

--- src/Database.php
+++ src/Database.php
@@ -12,7 +12,7 @@ class Database
     public function __construct()
     {
         $dbConfig = require base_path('src/config/database.php') ?? [];
-        $dsn = "mysql:host={$dbConfig['host']};port={$dbConfig['port']};dbname={$dbConfig['dbname']};charset={$dbConfig['charset']}";
+        $dsn = 'mysql:' . http_build_query($dbConfig, arg_separator: ';');
 
         try {
             $this->connection = new PDO($dsn, options: [
