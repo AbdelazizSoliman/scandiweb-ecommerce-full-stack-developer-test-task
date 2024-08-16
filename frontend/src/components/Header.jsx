1e57614704811ae6e9e7aff3fffe3e26fc5881f2
Abdelaziz Soliman <abdelaziz.soliman89@gmail.com>
2024-08-17 01:05:11 +0300

    Test

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -52,9 +52,9 @@ const Header = () => {
   useEffect(() => {
     if (cartModalVisible) {
       setShowModal(true);
-      setCartModalVisible(false);
     }
-  }, [cartModalVisible, setCartModalVisible]);
+  }, [cartModalVisible]);
+  
 
   if (dataError) {
     return (
